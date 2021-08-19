54ae5c1a1ca39ab49bb1e2e235096a2d86c4fabb
ludmi <ludmi@DESKTOP-SLQR66M>
2021-08-19 20:38:52 -0300

    maqueta inicial para el proyecto.

--- src/Atraccion.java
+++ src/Atraccion.java
@@ -1,7 +1,25 @@
 
-public class Atraccion {
-       private double costo;
+public class Atraccion 
+{
+       private tipoDeAtraccion tipoDeAtraccion;
+	   private String nombreDeLaAtraccion;
+	   private double costo;
        private double promedioDeTiempo;
        private int cupoDePersonasDisponibles;
        
-}
+       
+       
+	public Atraccion(tipoDeAtraccion tipoDeAtraccion, String nombreDeLaAtraccion, double costo, double promedioDeTiempo,
+			int cupoDePersonasDisponibles) {
+		super();
+		this.tipoDeAtraccion = tipoDeAtraccion;
+		this.nombreDeLaAtraccion = nombreDeLaAtraccion;
+		this.costo = costo;
+		this.promedioDeTiempo = promedioDeTiempo;
+		this.cupoDePersonasDisponibles = cupoDePersonasDisponibles;
+	}
+        
+       
+       
+        }
+
---
+++ src/Promocion.java
@@ -0,0 +1,8 @@
+
+public class Promocion {
+
+ private Atraccion atraccion;
+ 
+ 
+	
+}
