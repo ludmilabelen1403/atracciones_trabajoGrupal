262566f86da1ad7b9f1d0d2263684ce7a35718e0
ludmi <ludmi@DESKTOP-SLQR66M>
2021-08-21 20:32:04 -0300

    promoción absoluta

--- src/Promocion.java
+++ src/Promocion.java
@@ -14,6 +14,15 @@ public class Promocion {
 	 costoTotal-= (costoTotal*0.20);
 	 return costoTotal;
 }
- 
+ public double CalculaPromocionAbsoluta(Atraccion[] atracciones){
+	 double costoTotal = 0;
+	 for ( int i=0; i<atracciones.length;i++) {
+		 costoTotal+= atracciones[i].getCosto();
+	 }
+	 
+	 return costoTotal*0.75;
+	 /*le hace un 25% de descuento al paquete total)*/
+	 }
+
 
 }
