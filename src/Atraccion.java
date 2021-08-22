30ea313aadbbb0fc8bcacf3af8c7392ec3285f80
Juan Cruz <juan.teal.97@gmail.com>
2021-08-22 19:57:55 -0300

    "Version 2.0"

--- src/Atraccion.java
+++ src/Atraccion.java
@@ -1,44 +1,33 @@
+public abstract class Atraccion {
 
-public class Atraccion 
-{
-       private tipoDeAtraccion tipoDeAtraccion;
-	   private String nombreDeLaAtraccion;
-	   private double costo;
-       private double promedioDeTiempo;
-       private int cupoDePersonasDisponibles;
-       
-       
-      
-       
-       
-	public Atraccion(tipoDeAtraccion tipoDeAtraccion, String nombreDeLaAtraccion, double costo, double promedioDeTiempo,
-			int cupoDePersonasDisponibles) {
-		super();
-		this.tipoDeAtraccion = tipoDeAtraccion;
+	protected String nombreDeLaAtraccion;
+	protected double costo;
+	protected double promedioDeTiempo;
+	protected int cupoDePersonasDisponibles;
+
+	public Atraccion(String nombreDeLaAtraccion, double costo, double promedioDeTiempo, int cupoDePersonasDisponibles) {
+		
 		this.nombreDeLaAtraccion = nombreDeLaAtraccion;
 		this.costo = costo;
 		this.promedioDeTiempo = promedioDeTiempo;
 		this.cupoDePersonasDisponibles = cupoDePersonasDisponibles;
 	}
 
-
-
-	public tipoDeAtraccion getTipoDeAtraccion() {
-		return tipoDeAtraccion;
-	}
-
-
-	public String getNombreDeLaAtraccion() {
+	protected String getNombreDeLaAtraccion() {
 		return nombreDeLaAtraccion;
 	}
 
-
-
-	public double getCosto() {
+	protected double getCosto() {
 		return costo;
 	}
 
- 
-       
-        }
+	protected double getPromedioDeTiempo() {
+		return promedioDeTiempo;
+	}
+
+	protected int getCupoDePersonasDisponibles() {
+		return cupoDePersonasDisponibles;
+	}
+
+}
 
--- src/Usuario.java
+++ src/Usuario.java
@@ -3,6 +3,18 @@ public class Usuario {
 
 	private double presupuesto;
 	private double tiempoDisponible;
-	private String preferencia;
-	
+	private Atraccion preferencia;
+
+	protected double getPresupuesto() {
+		return presupuesto;
+	}
+
+	protected double getTiempoDisponible() {
+		return tiempoDisponible;
+	}
+
+	protected Atraccion getPreferencia() {
+		return preferencia;
+	}
+
 }
