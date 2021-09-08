6a124b3830c9c9cf340f28b8df3d6a6fd25b61ef
Juan Cruz <juan.teal.97@gmail.com>
2021-09-08 17:35:53 -0300

    Correcciones en lectores y batalla con exception

--- src/turismoTierraMedia/LectorDePromociones.java
+++ src/turismoTierraMedia/LectorDePromociones.java
@@ -20,18 +20,22 @@ public class LectorDePromociones {
 			while ((linea = br.readLine()) != null) {
 				String[] promocion = linea.split(",");
 				int index1 = 0, index2 = 0, index3 = 0;
+				String promoSubCero = promocion[0];
+				String promoSubUno = promocion[1];
+				String promoSubDos = promocion[2];
+				String promoSubTres = promocion[3];
 				tipo tipoDeAtraccion = null;
 
 				for (int i = 0; i < ListaDeAtracciones.size(); i++) {
-					if (promocion[1].strip().equals(ListaDeAtracciones.get(i).getNombre())) {
+					if (promoSubUno.toUpperCase().equals(ListaDeAtracciones.get(i).getNombre())) {
 						index1 = i;
 						tipoDeAtraccion = ListaDeAtracciones.get(i).getTipoDeAtraccion();
 					}
-					if (promocion[2].strip().equals(ListaDeAtracciones.get(i).getNombre())) {
+					if (promoSubDos.toUpperCase().equals(ListaDeAtracciones.get(i).getNombre())) {
 						index2 = i;
 						tipoDeAtraccion = ListaDeAtracciones.get(i).getTipoDeAtraccion();
 					}
-					if (promocion[3].strip().equals(ListaDeAtracciones.get(i).getNombre())) {
+					if (promoSubTres.toUpperCase().equals(ListaDeAtracciones.get(i).getNombre())) {
 						index3 = i;
 						tipoDeAtraccion = ListaDeAtracciones.get(i).getTipoDeAtraccion();
 					}
@@ -39,19 +43,19 @@ public class LectorDePromociones {
 				}
 				
 				Promocion p, d;
-				if (promocion[3].matches(".*\\d.*")) {
-	        		double valor = Double.parseDouble(promocion[3]);
+				if (promoSubTres.matches(".*\\d.*")) {
+	        		double valor = Double.parseDouble(promoSubTres);
 	        		if (valor < 1) {
-	        			p = new PromocionPorcentual(promocion[0], ListaDeAtracciones.get(index1), ListaDeAtracciones.get(index2), valor);
+	        			p = new PromocionPorcentual(promoSubCero, ListaDeAtracciones.get(index1), ListaDeAtracciones.get(index2), valor);
 	        			d = new PromocionPorcentual(p.getNombre(), p.getAtraccion1(), p.getAtraccion2(), valor, p.calcularPromocion(), p.calcularTiempo(), tipoDeAtraccion, Math.min(p.atraccion1.getCupo(), p.atraccion2.getCupo()));
 	        		}
 	        		else {
-	        			p = new PromocionAbsoluta(promocion[0], ListaDeAtracciones.get(index1), ListaDeAtracciones.get(index2), Double.parseDouble(promocion[3]));
+	        			p = new PromocionAbsoluta(promoSubCero, ListaDeAtracciones.get(index1), ListaDeAtracciones.get(index2), Double.parseDouble(promoSubTres));
 	        			d = new PromocionAbsoluta(p.getNombre(), p.getAtraccion1(), p.getAtraccion2(), p.calcularPromocion(), p.calcularPromocion(), p.calcularTiempo(), tipoDeAtraccion, Math.min(p.atraccion1.getCupo(), p.atraccion2.getCupo()));
 	        		}
 	        	}
 	        	else {
-	        		 p = new PromocionAxB(promocion[0], ListaDeAtracciones.get(index1), ListaDeAtracciones.get(index2), ListaDeAtracciones.get(index3));
+	        		 p = new PromocionAxB(promoSubCero, ListaDeAtracciones.get(index1), ListaDeAtracciones.get(index2), ListaDeAtracciones.get(index3));
 	        		 d = new PromocionAxB(p.getNombre(), p.getAtraccion1(), p.getAtraccion2(), ListaDeAtracciones.get(index3), p.calcularPromocion(), p.calcularTiempo(), tipoDeAtraccion, Math.min(Math.min(p.atraccion1.getCupo(),p.atraccion2.getCupo()),ListaDeAtracciones.get(index3).getCupo()));
 	        	}
 				
