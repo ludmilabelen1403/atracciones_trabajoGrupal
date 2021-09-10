39ed1191789239b8c8a1001072bf26025d8a0f64
Alana Mina <71946739+AlanaMina@users.noreply.github.com>
2021-09-10 18:49:13 -0300

    Si github coopera, el codigo se sube

--- src/turismoTierraMedia/Promocion.java
+++ src/turismoTierraMedia/Promocion.java
@@ -5,56 +5,52 @@ import java.util.Objects;
 
 
 public abstract class Promocion extends Producto{
-
 	protected String nombre;
 	protected Atraccion atraccion1;
 	protected Atraccion atraccion2;
 	protected tipo tipoDeAtraccion;
-	Atraccion[] atracciones;
-	protected int cupo;
+	Atraccion [] atracciones;
+	protected int cupo ;
 	protected Atraccion atraccionGratis;
-
+	
 	public Promocion(String nombre, double costo, double tiempo, tipo tipoDeAtraccion, int cupo) {
 		super(nombre, costo, tiempo, tipoDeAtraccion, cupo);
 	}
-  
+
 	protected Promocion(String nombre, Atraccion atraccion1, Atraccion atraccion2) {
 		super(nombre);
 		validarPromocion(atraccion1, atraccion2);
 	}
-
+	 
 	protected abstract double calcularPromocion();
-
+	
 	@Override
 	public String getNombre() {
 		return super.nombre;
 	}
-
+	
 	protected Atraccion getAtraccion1() {
 		return this.atraccion1;
 	}
-
+	
 	protected Atraccion getAtraccion2() {
 		return this.atraccion2;
 	}
-
+	
 	protected abstract double calcularTiempo();
-
+	
 	private void validarPromocion(Atraccion atraccion1, Atraccion atraccion2) {
 		try {
 			if (atraccion1.getTipoDeAtraccion() != atraccion2.getTipoDeAtraccion()) {
-
-				throw new TipoDeAtraccionException(
-						"La promoción no se puede realizar con dos tipos de atracciones diferentes");
+				throw new TipoAtraccionException ("Las atracciones deben ser del mismo tipo");
 			}
-
 			this.atraccion1 = atraccion1;
 			this.atraccion2 = atraccion2;
 			this.tipoDeAtraccion = atraccion1.getTipoDeAtraccion();
-		} catch (TipoDeAtraccionException tae) {
-			System.err.println(tae.getMessage());
 		}
-
+		catch (TipoAtraccionException tae){
+            System.err.println(tae.getMessage());
+        }
 	}
 
 	@Override
@@ -79,15 +75,17 @@ public abstract class Promocion extends Producto{
 				&& Arrays.equals(atracciones, other.atracciones) && Objects.equals(nombre, other.nombre)
 				&& tipoDeAtraccion == other.tipoDeAtraccion;
 	}
-
-	public abstract void restarCupo();
-
+	
+	public abstract void restarCupo() ;
 	public int getCupo() {
 		return cupo;
 	}
 
+	
+
 	public void setCupo(int cupo) {
 		this.cupo = cupo;
 	}
-
+	
+	
 }
